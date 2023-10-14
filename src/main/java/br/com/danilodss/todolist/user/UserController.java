b0487cfbc78d965df6e90c96790bd06faa74fa07
Danilo Silva <danilosantos14a@gmail.com>
2023-10-14 16:50:51 -0300

    User Safety

--- src/main/java/br/com/danilodss/todolist/user/UserController.java
+++ src/main/java/br/com/danilodss/todolist/user/UserController.java
@@ -1,10 +1,5 @@
 package br.com.danilodss.todolist.user;
-/*
- * Modificador
- * public
- * private
- * protected 
- */
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users") 
@@ -28,6 +25,10 @@ public class UserController {
         if(user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
